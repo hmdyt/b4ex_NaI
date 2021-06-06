c89b3ae1d9be89ab0a77ff7b0af5ae5396a02381
Hamada Yuto <yuto@MacBook-Air.local>
2021-06-06 21:35:46 +0900

    init and add ABC.C

---
+++ rootlogon.C
@@ -0,0 +1,18 @@
+void loadMacro(const TString macro_name){
+    // load
+    const int ret = gROOT->LoadMacro(macro_name);
+
+    if (ret == 0){
+        // load success
+        cout << "loaded " + macro_name << endl;
+    } else {
+        //load failed
+        cout << "faild " + macro_name << endl;
+        exit(1);
+    }
+}
+
+void rootlogon(){
+    cout << "executed rootlogon" << endl;
+    loadMacro("src/calibration/ABC.C");
+}
