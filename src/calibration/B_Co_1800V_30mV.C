335a84d7aa0063d2677fba2870eea15fc2fa3525
tomoe <t.peanuts0115@gmail.com>
2021-06-09 14:03:49 +0900

    changed fit of B and C

--- src/calibration/B_Co_1800V_30mV.C
+++ src/calibration/B_Co_1800V_30mV.C
@@ -12,47 +12,47 @@ void B_Co_1800V_30mV(){
 	c1->Draw();
 
 	const Double_t FitMin1 =1900 ;
-	const Double_t FitMax1 =2100 ;
-	const Double_t FitMin2 =2200 ;
-	const Double_t FitMax2 =2400 ;
+	const Double_t FitMax1 =2400 ;
+	//const Double_t FitMin2 =2200 ;
+	//const Double_t FitMax2 =2400 ;
 
-	TF1* func1 = new TF1("func1", "gaus", FitMin1, FitMax1);
-	TF1* func2 = new TF1("func2", "gaus", FitMin2, FitMax2);
+	TF1* func1 = new TF1("func1", "[0]*TMath::Gaus(x, [1], [2]) + [3]*TMath::Gaus(x, [4], [5])", FitMin1, FitMax1);
+	//TF1* func2 = new TF1("func2", "gaus", FitMin2, FitMax2);
 
-	func1->SetParName(0, "Const");
-	func1->SetParName(1, "Mean");
-	func1->SetParName(2, "Sigma");
+	func1->SetParName(0, "Const1");
+	func1->SetParName(1, "Mean1");
+	func1->SetParName(2, "Sigma1");
 
-	func2->SetParName(0, "Const");
-	func2->SetParName(1, "Mean");
-	func2->SetParName(2, "Sigma");
+	func1->SetParName(3, "Const2");
+	func1->SetParName(4, "Mean2");
+	func1->SetParName(5, "Sigma2");
 
 	func1->SetParameter(0,50);
 	func1->SetParameter(1,2000);
 	func1->SetParameter(2,50);
 
-	func2->SetParameter(0, 40);
-	func2->SetParameter(1, 2300);
-	func2->SetParameter(2, 50);
+	func1->SetParameter(3, 40);
+	func1->SetParameter(4, 2300);
+	func1->SetParameter(5, 50);
 
 	hist1->Fit("func1", "R", "same", FitMin1, FitMax1);
-	hist1->Fit("func2", "R+", "same", FitMin2, FitMax2);
+	//hist1->Fit("func2", "R+", "same", FitMin2, FitMax2);
 
 	func1->SetLineColor(kRed);
 	func1->Draw("same");
-	func2->SetLineColor(kRed);
-	func2->Draw("same");
+	//func2->SetLineColor(kRed);
+	//func2->Draw("same");
 
 	//Mean
 	Double_t y1 = func1->GetParameter(1);
-	Double_t y2 = func2->GetParameter(1);
+	Double_t y2 = func1->GetParameter(4);
 
 	//cout << x1 << endl;
 	//cout << x2 << endl;
 
 	//MeanError
 	Double_t a1 = func1->GetParError(1);
-	Double_t a2 = func2->GetParError(1);
+	Double_t a2 = func1->GetParError(4);
 	
 	Double_t x1 = 1173*1000;
 	Double_t x2 = 1333*1000;
